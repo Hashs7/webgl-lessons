3c164487407b08e2ee16f6d07b0f75b27f140fc9
Sebastien Hernoux <seb_her@hotmail.fr>
2020-03-13 11:23:55 +0100

    Create triangles

--- exercices/2.draw-triangle/index.js
+++ exercices/2.draw-triangle/index.js
@@ -6,30 +6,45 @@ import './style.scss';
 const canvas = document.getElementById("canvas");
 const gl = setupWebGL(canvas);
 let VERTEX_SHADER = `
-  attribute vec4 aPosition;
-  attribute float aPointSize;
+  attribute vec2 coordinates;
 
-  void main() {
-    gl_Position = vec4(aPosition);
-    gl_PointSize = aPointSize;
+  void main(void) {
+     gl_Position = vec4(coordinates, 0.0, 1.0);
   }
 `;
 let FRAGMENT_SHADER = `
   precision mediump float;
   uniform vec3 uColor;
 
-  void main() {
-    gl_FragColor = vec4(uColor, 1.0);
+  void main(void) {
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 0.1);
   }
 `;
 
 initShaders(gl, VERTEX_SHADER, FRAGMENT_SHADER);
-
-let aPosition = gl.getAttribLocation(gl.program, 'aPosition');
-let aPointSize = gl.getAttribLocation(gl.program, 'aPointSize');
+const coord = gl.getAttribLocation(gl.program, 'coordinates');
 let uColor = gl.getUniformLocation(gl.program, 'uColor');
 
 let POINTS_POSITION = [];
+
+function initTriangle() {
+  const vertices = [
+    -0.5,0.5,
+    -0.5,-0.5,
+    0.5,-0.5,
+    -1,0.2,
+    -1.0,-0.5,
+    0.7,-0.3,
+  ];
+
+  const vertex_buffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(coord);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
 
 function addPoint(e) {
   const x = (e.clientX / window.innerWidth) * 2 - 1;
@@ -48,27 +63,27 @@ function animate() {
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  initTriangle();
   POINTS_POSITION.forEach(({ position, size }) => {
     const floatArray = new Float32Array([position.x, position.y, 0.0]);
     const color = position.x < 0 ? [1.0, 0.0, 0.0] : [0.0, 0.0, 1.0];
     gl.vertexAttrib3fv(aPosition, floatArray);
     gl.vertexAttrib1f(aPointSize, size);
     gl.uniform3fv(uColor, color);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    return { position, size };
   });
 }
 
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 }
 
 resize();
 animate();
+initTriangle();
 
 window.addEventListener('resize', resize);
-window.addEventListener('mousemove', addPoint);
+// window.addEventListener('click', addPoint);
