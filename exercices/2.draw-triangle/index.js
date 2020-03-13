b8248b11ccc4ede45cd0ca52cef994bd08619edb
Sebastien Hernoux <seb_her@hotmail.fr>
2020-03-13 12:08:57 +0100

    Degrade colors

--- exercices/2.draw-triangle/index.js
+++ exercices/2.draw-triangle/index.js
@@ -1,61 +1,58 @@
 import { setupWebGL } from '../../lib/webgl-utils';
 import { initShaders } from '../../lib/webgl-shader-utils';
-import { randomNumber } from "../../utils";
 import './style.scss';
 
 const canvas = document.getElementById("canvas");
 const gl = setupWebGL(canvas);
 let VERTEX_SHADER = `
+  precision mediump float;
   attribute vec2 coordinates;
+  attribute vec3 aColor;
+  varying vec3 vColor;
 
   void main(void) {
      gl_Position = vec4(coordinates, 0.0, 1.0);
+     vColor = aColor;
   }
 `;
 let FRAGMENT_SHADER = `
   precision mediump float;
-  uniform vec3 uColor;
+  varying vec3 vColor;
 
   void main(void) {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 0.1);
+    gl_FragColor = vec4(vColor, 0.1);
   }
 `;
 
 initShaders(gl, VERTEX_SHADER, FRAGMENT_SHADER);
-const coord = gl.getAttribLocation(gl.program, 'coordinates');
-let uColor = gl.getUniformLocation(gl.program, 'uColor');
 
-let POINTS_POSITION = [];
+const coord = gl.getAttribLocation(gl.program, 'coordinates');
+const aColor = gl.getAttribLocation(gl.program, 'aColor');
 
 function initTriangle() {
   const vertices = [
-    -0.5,0.5,
-    -0.5,-0.5,
-    0.5,-0.5,
-    -1,0.2,
-    -1.0,-0.5,
-    0.7,-0.3,
+    -0.5, 0.5, 1, 0.5, 0.5,
+    -0.5, -0.5, 0.5, 0.0, 1,
+    0.5, -0.5, 1, 0.5, 0.5,
+    -1, 0.2, 1, 0.5, 0.5,
+    -1.0, -0.5, 0.5, 1, 0.5,
+    0.7, -0.3, 0.5, 0.0, 1,
   ];
 
-  const vertex_buffer = gl.createBuffer();
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+  const bpe = Float32Array.BYTES_PER_ELEMENT;
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 5 * bpe, 0);
   gl.enableVertexAttribArray(coord);
+
+  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * bpe, 2 * bpe);
+  gl.enableVertexAttribArray(aColor);
+
   gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
-function addPoint(e) {
-  const x = (e.clientX / window.innerWidth) * 2 - 1;
-  const y = -(e.clientY / window.innerHeight) * 2 + 1;
-  const size = randomNumber(5, 50);
-  POINTS_POSITION.push({ position : { x, y }, size });
-}
-
 function animate() {
-  POINTS_POSITION = POINTS_POSITION.filter(point => point.size > 0.1);
-  POINTS_POSITION = POINTS_POSITION.map(({ position, size }) => ({ position, size: size - 0.4 }));
   render();
   requestAnimationFrame(animate);
 }
@@ -64,15 +61,6 @@ function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   initTriangle();
-  POINTS_POSITION.forEach(({ position, size }) => {
-    const floatArray = new Float32Array([position.x, position.y, 0.0]);
-    const color = position.x < 0 ? [1.0, 0.0, 0.0] : [0.0, 0.0, 1.0];
-    gl.vertexAttrib3fv(aPosition, floatArray);
-    gl.vertexAttrib1f(aPointSize, size);
-    gl.uniform3fv(uColor, color);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-    return { position, size };
-  });
 }
 
 function resize() {
@@ -86,4 +74,3 @@ animate();
 initTriangle();
 
 window.addEventListener('resize', resize);
-// window.addEventListener('click', addPoint);
